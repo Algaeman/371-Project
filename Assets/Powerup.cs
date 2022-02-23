9ca61107bec4496b33559208c9b2ef0a14160166
gdesrosi00 <49248105+gdesrosi00@users.noreply.github.com>
2022-02-22 18:28:08 -0800

    Powerup Revisions
    Included new png for billboard as well as scripts

---
+++ Assets/Powerup.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    public float spinSpeed = 1f;
+
+    Vector3 _yRotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        float rand = Random.value - 0.5f;
+        _yRotation = Vector3.up * (spinSpeed + rand);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(_yRotation);
+    }
+}
